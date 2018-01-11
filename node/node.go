a49fdcf8a077145dc330ae79fc297c3f7619c052
z7zmey <z7zmey@gmail.com>
2018-01-11 19:37:53 +0200

    remove position from node package

--- node/node.go
+++ node/node.go
@@ -1,20 +1,7 @@
 package node
 
-import "fmt"
-
 // Node interface
 type Node interface {
 	Attributes() map[string]interface{}
 	Walk(v Visitor)
-}
-
-type Position struct {
-	StartLine int
-	EndLine   int
-	StartPos  int
-	EndPos    int
-}
-
-func (p Position) String() string {
-	return fmt.Sprintf("Pos{Line: %d-%d Pos: %d-%d}", p.StartLine, p.EndLine, p.StartPos, p.EndPos)
 }
