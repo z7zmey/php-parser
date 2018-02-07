038ebb719e90a3c119f8c42bc2092b064098c5a4
z7zmey <z7zmey@gmail.com>
2018-02-07 23:32:10 +0200

    break node test

---
+++ node/stmt/t_break_test.go
@@ -0,0 +1,79 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/scalar"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestBreakEmpty(t *testing.T) {
+	src := `<? while (1) { break; }`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.While{
+				Cond: &scalar.Lnumber{Value: "1"},
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{
+						&stmt.Break{nil},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+
+	assertEqual(t, expected, actual)
+}
+
+func TestBreakLight(t *testing.T) {
+	src := `<? while (1) { break 2; }`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.While{
+				Cond: &scalar.Lnumber{Value: "1"},
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{
+						&stmt.Break{
+							Expr: &scalar.Lnumber{Value: "2"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+
+	assertEqual(t, expected, actual)
+}
+
+func TestBreak(t *testing.T) {
+	src := `<? while (1) { break(3); }`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.While{
+				Cond: &scalar.Lnumber{Value: "1"},
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{
+						&stmt.Break{
+							Expr: &scalar.Lnumber{Value: "3"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+
+	assertEqual(t, expected, actual)
+}
