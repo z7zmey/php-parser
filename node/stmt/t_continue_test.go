6e532b31e021e82007d7c1bd812eecb05ddbce62
z7zmey <z7zmey@gmail.com>
2018-02-08 22:43:02 +0200

    Continue tests

---
+++ node/stmt/t_continue_test.go
@@ -0,0 +1,86 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/scalar"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestContinueEmpty(t *testing.T) {
+	src := `<? while (1) { continue; }`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.While{
+				Cond: &scalar.Lnumber{Value: "1"},
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{
+						&stmt.Continue{Expr: nil},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestContinueLight(t *testing.T) {
+	src := `<? while (1) { continue 2; }`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.While{
+				Cond: &scalar.Lnumber{Value: "1"},
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{
+						&stmt.Continue{
+							Expr: &scalar.Lnumber{Value: "2"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestContinue(t *testing.T) {
+	src := `<? while (1) { continue(3); }`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.While{
+				Cond: &scalar.Lnumber{Value: "1"},
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{
+						&stmt.Continue{
+							Expr: &scalar.Lnumber{Value: "3"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
