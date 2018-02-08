f34a939feea312ccbb17029b7cfb3790a8cdaf80
z7zmey <z7zmey@gmail.com>
2018-02-08 23:06:19 +0200

    Do tests

---
+++ node/stmt/t_do_test.go
@@ -0,0 +1,34 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/scalar"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestDo(t *testing.T) {
+	src := `<? do {} while(1);`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Do{
+				Stmt: &stmt.StmtList{
+					Stmts: []node.Node{},
+				},
+				Cond: &scalar.Lnumber{Value: "1"},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
