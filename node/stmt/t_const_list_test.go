c207947ad90fa8663f002fca107e86c81e067a88
z7zmey <z7zmey@gmail.com>
2018-02-08 22:38:38 +0200

    ConstList tests

---
+++ node/stmt/t_const_list_test.go
@@ -0,0 +1,42 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/scalar"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestConstList(t *testing.T) {
+	src := `<? const FOO = 1, BAR = 2;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.ConstList{
+				Consts: []node.Node{
+					&stmt.Constant{
+						PhpDocComment: "",
+						ConstantName:  &node.Identifier{Value: "FOO"},
+						Expr:          &scalar.Lnumber{Value: "1"},
+					},
+					&stmt.Constant{
+						PhpDocComment: "",
+						ConstantName:  &node.Identifier{Value: "BAR"},
+						Expr:          &scalar.Lnumber{Value: "2"},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
