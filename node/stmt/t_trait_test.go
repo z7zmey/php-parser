2f0480f8d689be1f4bdce9546795ed42bca04251
z7zmey <z7zmey@gmail.com>
2018-02-09 14:19:26 +0200

    Trait tests

---
+++ node/stmt/t_trait_test.go
@@ -0,0 +1,31 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+	
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestTrait(t *testing.T) {
+	src := `<? trait Foo {}`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Trait{
+				PhpDocComment: "",
+				TraitName: &node.Identifier{Value: "Foo"},
+				Stmts: []node.Node{},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
