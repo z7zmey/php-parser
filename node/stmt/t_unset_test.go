f668e34f4e50d44f434746fe810d4bab71228ad9
z7zmey <z7zmey@gmail.com>
2018-02-09 14:15:08 +0200

    Unset tests

---
+++ node/stmt/t_unset_test.go
@@ -0,0 +1,71 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+	
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/expr"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestUnset(t *testing.T) {
+	src := `<? unset($a);`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Unset{
+				Vars: []node.Node{
+					&expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestUnsetVars(t *testing.T) {
+	src := `<? unset($a, $b);`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Unset{
+				Vars: []node.Node{
+					&expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+					&expr.Variable{VarName: &node.Identifier{Value: "$b"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestUnsetTrailingComma(t *testing.T) {
+	src := `<? unset($a, $b,);`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Unset{
+				Vars: []node.Node{
+					&expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+					&expr.Variable{VarName: &node.Identifier{Value: "$b"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
