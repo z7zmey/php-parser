bdedb84f7c2ddd5efb245208b8e6317fc893c123
z7zmey <z7zmey@gmail.com>
2018-02-09 12:46:16 +0200

    InlinHtml tests

---
+++ node/stmt/t_inline_html_test.go
@@ -0,0 +1,29 @@
+
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestInlineHtml(t *testing.T) {
+	src := `<? ?> <div></div>`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Nop{},
+			&stmt.InlineHtml{Value: "<div></div>"},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
