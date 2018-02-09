cdbafcbf9ee82327166b527dd6727062eb1dd49f
z7zmey <z7zmey@gmail.com>
2018-02-09 12:41:55 +0200

    HaltCompiler tests

---
+++ node/stmt/t_halt_compiler_test.go
@@ -0,0 +1,28 @@
+
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestHaltCompiler(t *testing.T) {
+	src := `<? __halt_compiler();`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.HaltCompiler{},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
