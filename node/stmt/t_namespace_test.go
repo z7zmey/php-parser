ea421b414b1de38ee049e3d6a402894243876b17
z7zmey <z7zmey@gmail.com>
2018-02-09 13:46:05 +0200

    Namespace tests

---
+++ node/stmt/t_namespace_test.go
@@ -0,0 +1,76 @@
+package stmt_test
+
+import (
+	"github.com/z7zmey/php-parser/node/name"
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestNamespace(t *testing.T) {
+	src := `<? namespace Foo;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Namespace{
+				NamespaceName: &name.Name{
+					Parts: []node.Node{
+						&name.NamePart{Value: "Foo"},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestNamespaceStmts(t *testing.T) {
+	src := `<? namespace Foo {}`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Namespace{
+				NamespaceName: &name.Name{
+					Parts: []node.Node{
+						&name.NamePart{Value: "Foo"},
+					},
+				},
+				Stmts: []node.Node{},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+
+func TestAnonymousNamespace(t *testing.T) {
+	src := `<? namespace {}`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Namespace{
+				Stmts: []node.Node{},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
