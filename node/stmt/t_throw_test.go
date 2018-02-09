8b6f570de0ec26b7187f1a344e8a34ea30d0a035
z7zmey <z7zmey@gmail.com>
2018-02-09 14:08:28 +0200

    Throw tests

---
+++ node/stmt/t_throw_test.go
@@ -0,0 +1,30 @@
+package stmt_test
+
+import (
+	"bytes"
+	"testing"
+	
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/expr"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestThrow(t *testing.T) {
+	src := `<? throw $e;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Throw{
+				Expr: &expr.Variable{VarName: &node.Identifier{Value: "$e"}},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
