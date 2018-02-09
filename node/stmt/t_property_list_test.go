76d23a199c8b52688bffe647c96874953fd11769
z7zmey <z7zmey@gmail.com>
2018-02-09 13:55:02 +0200

    PropertiesList Property tests

---
+++ node/stmt/t_property_list_test.go
@@ -0,0 +1,81 @@
+package stmt_test
+
+import (
+	"github.com/z7zmey/php-parser/node/scalar"
+	"github.com/z7zmey/php-parser/node/expr"
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestProperty(t *testing.T) {
+	src := `<? class foo {var $a;}`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Class{
+				ClassName: &node.Identifier{Value: "foo"},
+				Stmts: []node.Node{
+					&stmt.PropertyList{
+						Modifiers: []node.Node{
+							&node.Identifier{Value: "var"},
+						},
+						Properties: []node.Node{
+							&stmt.Property{
+								PhpDocComment: "",
+								Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestProperties(t *testing.T) {
+	src := `<? class foo {public static $a, $b = 1;}`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Class{
+				ClassName: &node.Identifier{Value: "foo"},
+				Stmts: []node.Node{
+					&stmt.PropertyList{
+						Modifiers: []node.Node{
+							&node.Identifier{Value: "public"},
+							&node.Identifier{Value: "static"},
+						},
+						Properties: []node.Node{
+							&stmt.Property{
+								PhpDocComment: "",
+								Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+							},
+							&stmt.Property{
+								PhpDocComment: "",
+								Variable: &expr.Variable{VarName: &node.Identifier{Value: "$b"}},
+								Expr: &scalar.Lnumber{Value: "1"},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
