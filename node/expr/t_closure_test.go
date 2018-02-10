3ae13a3f04486af8270f1d249e1a9bcc22b1fc4a
z7zmey <z7zmey@gmail.com>
2018-02-10 11:27:20 +0200

    Clusure ClosureUse tests

---
+++ node/expr/t_closure_test.go
@@ -0,0 +1,82 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestClosure(t *testing.T) {
+	src := `<? function(){};`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.Closure{
+					ReturnsRef:    false,
+					Static:        false,
+					PhpDocComment: "",
+					Uses:          []node.Node{},
+					Stmts:         []node.Node{},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestClosureUse(t *testing.T) {
+	src := `<? function($a, $b) use ($c, &$d) {};`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.Closure{
+					ReturnsRef:    false,
+					Static:        false,
+					PhpDocComment: "",
+					Params: []node.Node{
+						&node.Parameter{
+							ByRef:    false,
+							Variadic: false,
+							Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+						},
+						&node.Parameter{
+							ByRef:    false,
+							Variadic: false,
+							Variable: &expr.Variable{VarName: &node.Identifier{Value: "$b"}},
+						},
+					},
+					Uses: []node.Node{
+						&expr.ClosureUse{
+							ByRef:    false,
+							Variable: &expr.Variable{VarName: &node.Identifier{Value: "$c"}},
+						},
+						&expr.ClosureUse{
+							ByRef:    true,
+							Variable: &expr.Variable{VarName: &node.Identifier{Value: "$d"}},
+						},
+					},
+					Stmts: []node.Node{},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
