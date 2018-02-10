f8e83c2b533a20857a946c3630c4175ab18428d2
z7zmey <z7zmey@gmail.com>
2018-02-10 14:21:47 +0200

    ShortArray tests

---
+++ node/expr/t_short_array_test.go
@@ -0,0 +1,90 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/scalar"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestShortArray(t *testing.T) {
+	src := `<? [];`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.ShortArray{
+					Items: []node.Node{},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestShortArrayItem(t *testing.T) {
+	src := `<? [1];`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.ShortArray{
+					Items: []node.Node{
+						&expr.ArrayItem{
+							ByRef: false,
+							Val:   &scalar.Lnumber{Value: "1"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestShortArrayItems(t *testing.T) {
+	src := `<? [1=>1, &$b,];`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.ShortArray{
+					Items: []node.Node{
+						&expr.ArrayItem{
+							ByRef: false,
+							Key:   &scalar.Lnumber{Value: "1"},
+							Val:   &scalar.Lnumber{Value: "1"},
+						},
+						&expr.ArrayItem{
+							ByRef: true,
+							Val:   &expr.Variable{VarName: &node.Identifier{Value: "$b"}},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
