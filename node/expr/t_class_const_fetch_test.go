30071cc3206092c0df8c45b83bd67845441b69bb
z7zmey <z7zmey@gmail.com>
2018-02-10 11:15:27 +0200

    ClassConstFetch tests

---
+++ node/expr/t_class_const_fetch_test.go
@@ -0,0 +1,40 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/name"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestClassConstFetch(t *testing.T) {
+	src := `<? Foo::Bar;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.ClassConstFetch{
+					Class: &name.Name{
+						Parts: []node.Node{
+							&name.NamePart{Value: "Foo"},
+						},
+					},
+					ConstantName: &node.Identifier{Value: "Bar"},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
