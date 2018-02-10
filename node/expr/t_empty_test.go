ecbdcec67ae1ba723e5c890a57cc03e283feeda5
z7zmey <z7zmey@gmail.com>
2018-02-10 11:49:46 +0200

    Empty tests

---
+++ node/expr/t_empty_test.go
@@ -0,0 +1,33 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestEmpty(t *testing.T) {
+	src := `<? empty($a);`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.Empty{
+					Expr: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
