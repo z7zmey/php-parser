e7ee9a9703d5959d0b7a00f84fd006ce34fa257d
z7zmey <z7zmey@gmail.com>
2018-02-10 14:36:19 +0200

    StaticCall tests

---
+++ node/expr/t_static_call_test.go
@@ -0,0 +1,93 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/name"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestStaticCall(t *testing.T) {
+	src := `<? Foo::bar();`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.StaticCall{
+					Class: &name.Name{
+						Parts: []node.Node{
+							&name.NamePart{Value: "Foo"},
+						},
+					},
+					Call:      &node.Identifier{Value: "bar"},
+					Arguments: []node.Node{},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestStaticCallRelative(t *testing.T) {
+	src := `<? namespace\Foo::bar();`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.StaticCall{
+					Class: &name.Relative{
+						Parts: []node.Node{
+							&name.NamePart{Value: "Foo"},
+						},
+					},
+					Call:      &node.Identifier{Value: "bar"},
+					Arguments: []node.Node{},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestStaticCallFullyQualified(t *testing.T) {
+	src := `<? \Foo::bar();`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.StaticCall{
+					Class: &name.FullyQualified{
+						Parts: []node.Node{
+							&name.NamePart{Value: "Foo"},
+						},
+					},
+					Call:      &node.Identifier{Value: "bar"},
+					Arguments: []node.Node{},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
