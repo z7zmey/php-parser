3dbab3b2a762a76ee0bc66a584f2d60ec287d097
z7zmey <z7zmey@gmail.com>
2018-02-10 11:58:41 +0200

    update Clone tests

--- node/expr/t_clone_test.go
+++ node/expr/t_clone_test.go
@@ -12,8 +12,28 @@ import (
 	"github.com/z7zmey/php-parser/php7"
 )
 
-func TestClone(t *testing.T) {
+func TestCloneBrackets(t *testing.T) {
 	src := `<? clone($a);`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.Clone{
+					Expr: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestClone(t *testing.T) {
+	src := `<? clone $a;`
 
 	expected := &stmt.StmtList{
 		Stmts: []node.Node{
