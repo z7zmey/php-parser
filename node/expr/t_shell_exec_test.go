398645631ff90bff88465cd8c690bb93b61ecf0a
z7zmey <z7zmey@gmail.com>
2018-02-10 14:19:50 +0200

    ShallExec tests

---
+++ node/expr/t_shell_exec_test.go
@@ -0,0 +1,38 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/scalar"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestShellExec(t *testing.T) {
+	src := "<? `cmd $a`;"
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.ShellExec{
+					Parts: []node.Node{
+						&scalar.EncapsedStringPart{Value: "cmd "},
+						&expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+					},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
