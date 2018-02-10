2745cf4c266c51998daa3e3acc8ca7d0f38850fa
z7zmey <z7zmey@gmail.com>
2018-02-10 13:49:14 +0200

    PropertyFetch tests

---
+++ node/expr/t_property_fetch_test.go
@@ -0,0 +1,34 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestPropertyFetch(t *testing.T) {
+	src := `<? $a->foo;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.PropertyFetch{
+					Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+					Property: &node.Identifier{Value: "foo"},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
