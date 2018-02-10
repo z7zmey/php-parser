647207e97b566044a5d57adcf8bf9f7bad969cc7
z7zmey <z7zmey@gmail.com>
2018-02-10 13:43:52 +0200

    PostDec PostInc PreDec PreInc tests

---
+++ node/expr/t_inc_dec_test.go
@@ -0,0 +1,93 @@
+package expr_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/z7zmey/php-parser/node/expr"
+
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/stmt"
+	"github.com/z7zmey/php-parser/php5"
+	"github.com/z7zmey/php-parser/php7"
+)
+
+func TestPostDec(t *testing.T) {
+	src := `<? $a--;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.PostDec{
+					Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestPostInc(t *testing.T) {
+	src := `<? $a++;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.PostInc{
+					Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestPreDec(t *testing.T) {
+	src := `<? --$a;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.PreDec{
+					Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
+
+func TestPreInc(t *testing.T) {
+	src := `<? ++$a;`
+
+	expected := &stmt.StmtList{
+		Stmts: []node.Node{
+			&stmt.Expression{
+				Expr: &expr.PreInc{
+					Variable: &expr.Variable{VarName: &node.Identifier{Value: "$a"}},
+				},
+			},
+		},
+	}
+
+	actual, _, _ := php7.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+
+	actual, _, _ = php5.Parse(bytes.NewBufferString(src), "test.php")
+	assertEqual(t, expected, actual)
+}
