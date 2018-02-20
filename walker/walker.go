d320208aff7d19db856ba194a6cea89ac05ee3db
z7zmey <z7zmey@gmail.com>
2018-02-20 20:06:44 +0200

    update dumper

--- walker/walker.go
+++ walker/walker.go
@@ -11,5 +11,5 @@ type Walkable interface {
 type Visitor interface {
 	EnterNode(w Walkable) bool             // EnterNode is invoked for each node encountered by Walk.
 	GetChildrenVisitor(Key string) Visitor // GetChildrenVisitor is invoked at every node parameter that contains children nodes
-	LeaveNode(w Walkable)                  // LeaveNode is invoked after process node
+	LeaveNode(w Walkable)                  // LeaveNode is invoked after node processed
 }
